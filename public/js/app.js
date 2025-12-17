04fdd50369b67c91e02930565181024ce936ed55
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-17 20:35:59 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -4,22 +4,22 @@ function login() {
 
   fetch("/api/auth", {
     method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ email, password })
   })
   .then(res => res.json())
   .then(data => {
-    if (data.token) {
+    if(data.token) {
+      // حفظ التوكن في localStorage
       localStorage.setItem("token", data.token);
+      // الانتقال لصفحة المرضى
       window.location.href = "patients.html";
     } else {
       alert(data.error || "Login failed");
     }
   })
   .catch(err => {
-    console.error(err);
+    console.error("Login error:", err);
     alert("Server error");
   });
 }
