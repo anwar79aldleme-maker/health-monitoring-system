dcfacf3522bcb7e32e0c6e6133242e03d872ab94
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-17 19:27:17 +0300

    Update middleware.js

--- backend/api/middleware.js
+++ backend/api/middleware.js
@@ -1,1 +1,14 @@
+import jwt from "jsonwebtoken";
+
+export function verifyToken(req,res){
+  const authHeader = req.headers.authorization;
+  if(!authHeader) throw new Error("No token");
+
+  const token = authHeader.split(" ")[1];
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw new Error("Invalid token");
+  }
+}
 
