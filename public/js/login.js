f15c520ee01984cb900df17610ee644e9dcd42f6
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-18 05:27:53 +0000

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -1,21 +1,18 @@
-function login() {
-  fetch("/api/auth", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      email: email.value,
-      password: password.value
-    })
+
+function login(){
+ fetch("/api/auth",{
+  method:"POST",
+  headers:{"Content-Type":"application/json"},
+  body:JSON.stringify({
+    email:email.value,
+    password:password.value
   })
-  .then(r => r.json())
-  .then(d => {
-    if (d.success) {
-      localStorage.setItem("logged", "true");
-      location.href = "patients.html";
-    } else {
-      alert(d.error);
-    }
-  });
+ })
+ .then(r=>r.json())
+ .then(d=>{
+  if(d.success){
+    localStorage.setItem("logged","true");
+    location.href="patients.html";
+  }else alert(d.error);
+ });
 }
