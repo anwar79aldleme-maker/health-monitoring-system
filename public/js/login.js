18d102b3f5f80b503c8febc5dc399127ce0a39ca
anwar79aldleme-maker <anwar79aldleme@gmail.com>
2025-12-18 05:01:13 +0000

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -5,17 +5,18 @@ function login() {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-      email: document.getElementById("email").value,
-      password: document.getElementById("password").value
+      email: document.getElementById("email").value.trim(),
+      password: document.getElementById("password").value.trim()
     })
   })
   .then(res => {
-    if (res.status === 401) throw new Error("Wrong email or password");
+    if (res.status === 401) throw "Wrong email or password";
+    if (!res.ok) throw "Server error";
     return res.json();
   })
   .then(data => {
     localStorage.setItem("token", data.token);
-    window.location.href = "patients.html";
+    location.href = "patients.html";
   })
-  .catch(err => alert(err.message));
+  .catch(err => alert(err));
 }
